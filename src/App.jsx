506c16d50ab58ba65784ea8e131b8b985cdb767c
doniyorbek <doniyorbekmirkomilov1@gmail.com>
2025-10-01 13:09:23 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+     <Button variant="contained">Contained</Button>
+    </>
+  )
+}
+
+export default App
